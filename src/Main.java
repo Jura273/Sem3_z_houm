1143d4eb765c6315c5414bc7c09370384f6b3347
Юра <yusckorobogatov@yandex.ru>
2023-12-11 17:09:26 +0400

    Выполнена контрольная домашка к семинару №3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,99 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//    Напишите приложение, которое будет запрашивать у
+//    пользователя следующие данные в произвольном порядке,
+//    разделенные пробелом: Фамилия Имя Отчество, дата_рождения,
+//    номер_телефона, пол
+//    Форматы данных:фамилия, имя, отчество - строки дата_рождения
+//    - строка формата dd.mm.yyyy
+//     номер_телефона - целое беззнаковое число без форматирования
+//     пол - символ латиницей f или m.
+//     Критерии:
+//      Приложение должно проверить введенные данные по количеству.
+//     Если количество не совпадает с требуемым,
+//     вернуть код ошибки, обработать его и показать пользователю сообщение,
+//     что он ввел меньше и больше
+//      данных, чем требуется.
+//       Приложение должно попытаться распарсить полученные значения
+//      и выделить из них требуемые параметры.
+//      Если форматы данных не совпадают, нужно бросить исключение,
+//      соответствующее типу проблемы. Можно
+//      использовать встроенные типы java и создать свои.
+//      Исключение должно быть корректно обработано,
+//      пользователю выведено сообщение с информацией, что именно неверно.
+//         Если всё введено и обработано верно, должен создаться файл
+//        с названием, равным фамилии, в него в одну
+//        строку должны записаться полученные данные, вида.
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные пользователя в следующем формате:" +
+                " Фамилия Имя Отчество датаРождения НомерТелефона Пол");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String input = scanner.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Разделение введенных данных по пробелам
+
+        String[] userData = input.split(" ");
+
+        // Проверка количества данных
+
+        if (userData.length != 6) {
+            if (userData.length < 6) {
+                System.out.println("Вы ввели меньше данных, чем требуется");
+            } else {
+                System.out.println("Вы ввели больше данных, чем требуется");
+            }
+            return;
+        }
+
+        try {
+
+            String lastName = userData[0];
+            String firstName = userData[1];
+            String middleName = userData[2];
+            String dateOfBirth = userData[3];
+            String phoneNumber = userData[4];
+            char gender = userData[5].charAt(0);
+
+
+            // Проверка формата данных
+
+            boolean isValid = true;
+
+            if (!dateOfBirth.matches("\\d{2}.\\d{2}.\\d{4}")) {
+                System.out.println("Дата рождения должна быть указана в формате dd.mm.yyyy");
+                isValid = false;
+            }
+            if (gender != 'f' && gender != 'm') {
+                System.out.println("Пол должен быть указан символом 'f' или 'm'.");
+                isValid = false;
+            }
+            if (!isValid) {
+                return;
+            }
+
+            String fileName = Arrays.toString(new String[]{lastName}) + ".txt";
+
+            FileWriter fileWriter = new FileWriter(fileName, true);
+
+            // открываем файл на запись (true - дописать в файл)
+
+            fileWriter.write(Arrays.toString(new String[]{lastName}) + " " + Arrays.toString(new String[]{firstName}) + " " + Arrays.toString(new String[]{middleName}) + " " + Arrays.toString(new String[]{dateOfBirth}) + " " + Arrays.toString(new String[]{phoneNumber}) + " " + Arrays.toString(new char[]{gender}));
+
+            //закрываем соединение с файлом
+
+            fileWriter.close();
+
+            System.out.println("Данные успешно записаны ив файл !");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
